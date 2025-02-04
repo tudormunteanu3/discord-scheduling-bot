14d092c8803b60f14f94f89e6657a001e3d1f0e4
TudorMunteanu <tudor02m@gmail.com>
2025-02-04 14:12:52 +0000

    Added pomodoro.js command in commands/utility that when executed creates an instance of pomodoro in the channel that the command is called in using the name of the message author who called the command.

---
+++ commands/utility/pomodoro.js
@@ -0,0 +1,20 @@
+const { Pomodoro } = require('../../pomodoro.js');
+
+module.exports = {
+    data: {
+    name: 'pomodoro',
+    },
+    description: 'Creates a pomodoro timer. Use: !pomodoro <time in minutes>',
+
+    async execute(message, args) {
+        let defaultDuration = 25;
+        if (args.length >= 1) {
+            const parsed = parseInt(args[0], 10);
+            if (!isNaN(parsed)) {
+                defaultDuration = parsed;
+            }
+        }
+        const pomodoro = new Pomodoro(defaultDuration);
+        pomodoro.startSession(message.channel, message.author.username)
+    }
+}
